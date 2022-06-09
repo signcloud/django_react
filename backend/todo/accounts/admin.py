a895680d32f55b81865dbdbb2a82745ec22434e8
Dmitry Zhdanov <signmark@gmail.com>
2022-06-09 12:52:16 +0300

    Update admin user and fix api/v1/urls.py

--- backend/todo/accounts/admin.py
+++ backend/todo/accounts/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', "email", 'first_name', 'last_name')
+    list_display_links = ('id',)
+    list_editable = ("email", 'first_name', 'last_name')
+
+
+admin.site.register(User, UserAdmin)
 # Register your views here.
