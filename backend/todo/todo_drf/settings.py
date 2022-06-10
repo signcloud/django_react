0f677f0ebbdafb62f84aae0c0a020a5424843513
Dmitry Zhdanov <signmark@gmail.com>
2022-06-10 13:39:19 +0300

    Some fixes

--- backend/todo/todo_drf/settings.py
+++ backend/todo/todo_drf/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../../frontend/todo-app/build/static/')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, '../../frontend/todo-app/build/static/')
+# ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
